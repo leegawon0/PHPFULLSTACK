ba7fb8218725eef002576431722c4073d8ba86ca
leegawon0 <kaepang07@naver.com>
2023-03-27 18:10:29 +0900

    23.03.27

--- PHP/Example/05_1_array.php
+++ PHP/Example/05_1_array.php
@@ -66,8 +66,54 @@
 
 // 배열의 원소 삭제 : unset()
 
-$arr_week = array("Sun", "Mon", "delete", "Tue", "Wed");
-unset($arr_week[2]);
-print_r($arr_week);
+// $arr_week = array("Sun", "Mon", "delete", "Tue", "Wed");
+// unset($arr_week[2]);
+// print_r($arr_week);
+
+// 중복되지 않는 원소를 반환 : array_diff()
+// $arr_diff_1 = array("a", "b", "C");
+// $arr_diff_2 = array("a", "b", "d");
+// $arr_diff = array_diff($arr_diff_1, $arr_diff_2);
+// print_r($arr_diff);
+
+// 배열의 정렬 : asort(), arsort(), ksort(), krsort();
+// $arr_asort = array("b", "a", "d", "c");
+// asort($arr_asort);
+// print_r($arr_asort);
+
+// echo "\n";
+
+// arsort($arr_asort);
+// print_r($arr_asort);
+
+// echo "\n";
+
+// ksort($arr_asort);
+// print_r($arr_asort);
+
+// echo "\n";
+
+// krsort($arr_asort);
+// print_r($arr_asort);
+
+// echo "\n";
+
+// array의 사이즈를 반환하는 함수 : count()
+// echo count($arr_asort);
+
+// foreach($array as $key => $val){}
+// foreach($array as $val){}
+$arr1 = array("key1" => "val1", "key2" => "val2", "key3" => "val3", "key4" => "val4");
+foreach( $arr1 as $key => $val )
+{
+    echo $key." : ".$val."\n";
+}
+
+echo "\n";
+
+foreach( $arr1 as $val )
+{
+    echo $val."\n";
+}
 
 ?>
--- PHP/Training/05_1_array.php
+++ PHP/Training/05_1_array.php
@@ -19,15 +19,36 @@
 // echo $food["김밥"];
 
 // 키: 라면 원소를 삭제해 주세요
-$arr_food_del = array(
-    "떡볶이" => "떡"
-    , "라면" => "면"
-    , "김밥" => "김"
-    , "비빔만두" => "만두"
-);
-unset($arr_food_del["라면"]);
-print_r ($arr_food_del);
+// $arr_food_del = array(
+//     "떡볶이" => "떡"
+//     , "라면" => "면"
+//     , "김밥" => "김"
+//     , "비빔만두" => "만두"
+// );
+// unset($arr_food_del["라면"]);
+// print_r ($arr_food_del);
 
-sfasdfsdf
+// foreach문을 이용해서 array에서 키가 "삭제"인 요소를 제거해 주세요.
+// if문 사용, 키가 "삭제" 이외는 "키 : 값" 포맷으로 출력
+$food = array(
+                "떡볶이" => "떡"
+                , "라면" => "면"
+                , "삭제" => "값값"
+                , "김밥" => "김"
+                , "비빔만두" => "만두"
+);
+
+foreach ($food as $key => $val) {
+    if ($key == "삭제")
+    {
+        unset($food[$key]);
+    }
+    else
+    {
+        echo $key." : ".$val."\n";
+    }
+}
+
+print_r($food);
 
 ?>
